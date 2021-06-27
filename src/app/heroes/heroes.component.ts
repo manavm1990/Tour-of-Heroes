80c98e6cbe92e95a8ff9c67c25e2fb7f1893d673
Manav Misra <manavm@visionwebsoft.com>
2021-06-27 12:01:38 -0500

    use injected Heroes 🦸🏾‍♂️

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
-import { HEROES } from '../mock-heroes';
-import {Hero} from '../hero'
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 
 // Annotate our class with necessary metadata 👇🏾 using the Component DECORATOR from angular core
 @Component({
@@ -9,10 +9,23 @@ import {Hero} from '../hero'
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css'],
 })
-export class HeroesComponent {
-  heroes = HEROES;
+export class HeroesComponent implements OnInit {
+  heroes: Hero[] = [];
   selectedHero?: Hero;
   onSelect(hero: Hero) {
     this.selectedHero = hero;
+  }
+
+  constructor(
+    // private property heroService is an injection 💉 site for `HeroService` (singleton 1️⃣)
+    private heroService: HeroService
+  ) {}
+
+  getHeroes() {
+    this.heroes = this.heroService.getHeroes();
+  }
+
+  ngOnInit() {
+    this.getHeroes();
   }
 }
