064f212397496005e5d08acf360e30955edf164b
Manav Misra <manavm@visionwebsoft.com>
2021-06-27 16:40:08 -0500

    Add HeroService.getHero()

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,15 +1,23 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
 import { Hero } from './hero';
+import { MessageService } from './message.service';
 import { HEROES } from './mock-heroes';
-import {Observable, of } from 'rxjs'
-import {MessageService} from './message.service'
 
 @Injectable({
   // Provide this service throughout the app
   // Generally, always do like this one
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HeroService {
+  getHero(id: number): Observable<Hero> {
+    // ! means get the fuck on TS - it's not going to be null/undefined
+    // TODO: Handle errors
+    const hero = HEROES.find((hero) => hero.id === id)!;
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(hero);
+  }
+
   getHeroes(): Observable<Hero[]> {
     // of(HEROES) returns an Observable<Hero[]> that emits a single value, the array of mock heroes.
     // Observable is to manage asynchronous 💩
