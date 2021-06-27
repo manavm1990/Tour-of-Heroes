79b67553254ae40b7d7e586fe503af752440fdfb
Manav Misra <manavm@visionwebsoft.com>
2021-06-27 16:39:51 -0500

    :construction: Extract `id` router parameter

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,12 +1,36 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
   styleUrls: ['./hero-detail.component.css'],
 })
-export class HeroDetailComponent {
+export class HeroDetailComponent implements OnInit {
   // Decorate this component with the necessary Hero (or not)
   @Input() hero?: Hero;
+
+  getHero() {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.heroService.getHero(id).subscribe((hero) => {
+      this.hero = hero;
+    });
+  }
+
+  constructor(
+    // Has info about the route (e.g. 'id' parameter)
+    private route: ActivatedRoute,
+
+    // Get info about hero 🦸🏾‍♂️
+    private heroService: HeroService,
+
+    // Used to navigate back to view that came here
+    private location: Location
+  ) {}
+
+  ngOnInit(): void {
+    this.getHero();
+  }
 }
