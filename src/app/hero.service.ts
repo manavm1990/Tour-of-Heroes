f9e6b2519b4ac78cce80222d7edeb20e2d3f0134
Manav Misra <manavm@visionwebsoft.com>
2021-06-27 11:46:00 -0500

    :construction: Add `getHeroes` to `HeroService`

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
 
 @Injectable({
+  // Provide this service throughout the app
+  // Generally, always do like this one
   providedIn: 'root'
 })
 export class HeroService {
-
-  constructor() { }
+  getHeroes(): Hero[] {
+    return HEROES
+  }
 }
