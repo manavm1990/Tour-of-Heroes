33415696a8fadafed12cf72c57ce34f310d34007
Manav Misra <manavm@visionwebsoft.com>
2021-06-27 16:45:13 -0500

    :bug: Add missing Location import

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,3 +1,4 @@
+import { Location } from '@angular/common';
 import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Hero } from '../hero';
